a5af0f7a9df28ef6f6333a625d86448ee6050d57
liuqi.hx <qi.liu@dhc.com.cn>
2016-07-20 16:04:19 +0800

    删除无用引用

--- src/main/java/com/example/PageController.java
+++ src/main/java/com/example/PageController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.example.dao.StudentDao;
 import com.example.entity.Student;
 
 @Controller
